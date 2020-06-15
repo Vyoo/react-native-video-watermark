7e3788c4d109caa48756d2ef69e4a129c2002e5c
Tushar <freelancetushar@gmail.com>
2020-06-15 17:30:40 +0530

    Viideo Sampler added for resize

--- android/src/main/java/com/vyoo/VideoWatermarkModule.java
+++ android/src/main/java/com/vyoo/VideoWatermarkModule.java
@@ -8,7 +8,8 @@ import com.facebook.react.bridge.Callback;
 import com.daasuu.mp4compose.composer.Mp4Composer;
 import com.daasuu.mp4compose.filter.GlWatermarkFilter;
 import com.daasuu.mp4compose.FillMode;
-
+import com.vyoo.commonvideolibrary.SamplerClip;
+import com.vyoo.commonvideolibrary.VideoResampler;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -16,6 +17,11 @@ import java.io.IOException;
 import android.net.Uri;
 import android.util.Log;
 import android.graphics.BitmapFactory;
+
+import com.coremedia.iso.boxes.Container;
+import com.googlecode.mp4parser.authoring.Movie;
+import com.googlecode.mp4parser.authoring.builder.DefaultMp4Builder;
+import com.googlecode.mp4parser.authoring.container.mp4.MovieCreator;
 
 public class VideoWatermarkModule extends ReactContextBaseJavaModule {
 
@@ -37,7 +43,29 @@ public class VideoWatermarkModule extends ReactContextBaseJavaModule {
   }
 
   public void watermarkVideoWithImage(String videoPath, String imagePath, int width, int height, final Callback successCallback, final Callback failureCallback) {
-    File destFile = new File(this.getReactApplicationContext().getFilesDir(), "converted.mp4");    
+
+    File samplerDestFile = new File(this.getReactApplicationContext().getFilesDir(), "converted.mp4");
+      if (!samplerDestFile.exists()) {
+          try {
+              samplerDestFile.createNewFile();
+          } catch (IOException e) {
+              e.printStackTrace();
+          }
+      }
+      final String samplerDestinationPath = samplerDestFile.getPath();
+
+      VideoResampler resampler = new VideoResampler();
+
+      resampler.addSamplerClip(new SamplerClip(Uri.parse(videoPath)));
+      resampler.setOutput(Uri.parse(samplerDestinationPath));
+      resampler.setOutputResolution(width, height);
+      try {
+          resampler.start();
+      } catch (Throwable e) {
+          e.printStackTrace();
+      }
+      //successCallback.invoke(destinationPath);
+      File destFile = new File(this.getReactApplicationContext().getFilesDir(), "converted1.mp4");
       if (!destFile.exists()) {
           try {
               destFile.createNewFile();
@@ -45,9 +73,9 @@ public class VideoWatermarkModule extends ReactContextBaseJavaModule {
               e.printStackTrace();
           }
       }
-      final String destinationPath = destFile.getPath();      
+      final String destinationPath = destFile.getPath();
       try {
-          new Mp4Composer(Uri.fromFile(new File(videoPath)), destinationPath, reactContext)
+          new Mp4Composer(Uri.fromFile(samplerDestFile), destinationPath, reactContext)
                   .filter(new GlWatermarkFilter(BitmapFactory.decodeStream(reactContext.getContentResolver().openInputStream(Uri.fromFile(new File(imagePath))))))                  
                   .listener(new Mp4Composer.Listener() {
                       @Override
@@ -69,7 +97,7 @@ public class VideoWatermarkModule extends ReactContextBaseJavaModule {
                       public void onFailed(Exception exception) {
                           Log.e("Progress", "Failure" + "");
                           failureCallback.invoke("failed");
-                          //exception.printStackTrace();
+                          exception.printStackTrace();
                       }
                   }).start();
       } catch (FileNotFoundException e) {
